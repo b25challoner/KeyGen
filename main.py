80b0c7b58e899da9759bd71b3ab5213f7e1250e2
438571894 <113037796+FirstLast213@users.noreply.github.com>
2022-10-03 15:14:51 +0000

    output to text file

--- main.py
+++ main.py
@@ -2,20 +2,23 @@
 import random
 import string
 
-print(
-  "Optional:\n - Use this tool offline\n - Store the keys encrypted"
-)
+print("Optional:\n - Use this tool offline\n - Store the keys encrypted")
 length = int(input('\nKey Length: '))
 amount = int(input('\nNumber of Keys: '))
 print("")
 count = 1
-while count <= amount:
-  lower = string.ascii_lowercase
-  upper = string.ascii_uppercase
-  num = string.digits
-  symbols = string.punctuation
-  all = lower + upper + num + symbols
-  temp = random.sample(all, length)
-  password = "".join(temp)
-  print(password)
-  count = count + 1
+with open("keys.txt", "w") as f:
+  while count <= amount:
+    lower = string.ascii_lowercase
+    upper = string.ascii_uppercase
+    num = string.digits
+    symbols = string.punctuation
+    all = lower + upper + num + symbols
+    temp = random.sample(all, length)
+    key = "".join(temp)
+    print(key, file=f)
+    count = count + 1
+f = open('keys.txt', 'r')
+file_contents = f.read()
+print(file_contents)
+f.close()
